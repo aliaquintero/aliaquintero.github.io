a3edb0b9913af72437feac341f83bff5c6e59ee6
aliaquintero <aliaquintero@protonmail.com>
2022-12-27 15:55:01 -0500

    updated html

---
+++ app.js
@@ -0,0 +1,14 @@
+// TODO:
+// * Hero
+//     - Animated abstract graphic
+//     - Animated arrow showing site flow (scroll) Click to go to next section
+// * Projects
+//     - Add stack tags below project description (#react, #css, #javascript, etc.)
+//     - Transition fade in each individual project (scroll timer thing)
+//     - Fix spacing before contact form
+// * Contact
+//     - Hook up to Netlify forms
+//     - Transition fade in on scroll
+//     - When submitted, change to remove form + verify message sent + prevent multi-submission
+// * Footer
+//     - Center offiste icons vertically
